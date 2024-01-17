32ed1d731933889a8f8d827e24512b24ca1b0841
dannybanie <danielbrooks0000@gmail.com>
2024-01-17 17:58:59 +0100

    linked to mongodb successfully

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-import express from 'express';
+    import express from 'express';
+    import mongoose from 'mongoose';
+    import dotenv from 'dotenv';
 
-const app = express();
+    dotenv.config();
 
-app.listen(3000, () => {
-    console.log('Server is running on Port 3000')
-});
+    mongoose
+    .connect(process.env.MONGO)
+    .then(()=> {
+        console.log('Mongodb connected')
+    })
+    .catch(err => {
+        console.log('this is the error -> ' + err)
+    });
+
+    const app = express();
+
+    app.listen(3000, () => {
+        console.log('Server is running on Port 3000')
+    });
