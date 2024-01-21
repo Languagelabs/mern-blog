1f8e53982d48d7799eb0649598d3ccdb84466968
dannybanie <danielbrooks0000@gmail.com>
2024-01-21 15:41:45 +0100

    signin api created and tested working

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@
     });
 
     app.use('/api/user', userRoutes);
-    app.use('/api/auth', authRoutes)
+    app.use('/api/auth', authRoutes) 
 
     app.use((err, req, res, next) => {
         const statusCode = err.statusCode || 500;
